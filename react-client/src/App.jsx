c1dab69ec14710a13aa602003bf1835e00b33ae9
Carthicc <78253922+Carthicc@users.noreply.github.com>
2025-06-13 00:58:19 +0530

    Initial commit

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://karlynn.onrender.com");
 
 export default function App() {
   const [roomId, setRoomId] = useState("");
