a4e3923f2d90cf290eb41b72c6ce363908081e49
Carthicc <78253922+Carthicc@users.noreply.github.com>
2025-06-13 00:54:40 +0530

    Initial commit

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -46,5 +46,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log("🚀 Signaling server running on port ${PORT}");
+  console.log(`🚀 Signaling server running on port ${PORT}`);
 });
